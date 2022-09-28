fe7aa71d0e2fd1367c761d54dca603b6f2decc0c
hopfufu <91218375+hopfufu@users.noreply.github.com>
2022-09-28 13:41:45 +0530

    Nav-bar added

--- LibApp/views.py
+++ LibApp/views.py
@@ -1,4 +1,5 @@
 from email.mime import image
+from optparse import Values
 from django.shortcuts import render
 import pickle
 import os
@@ -7,8 +8,9 @@ def index(request):
     with open('popular1.pkl', 'rb') as f:
 
         popular_df=pickle.load(f)
+    
     context_dic={
         'imgUrl':list(popular_df['Image-URL-L']),
     }
-
+        
     return render(request,'index.html',context=context_dic)
