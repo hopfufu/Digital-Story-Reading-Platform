374c51248f71720a6039bcbd1b580b4a775f8402
hopfufu <91218375+hopfufu@users.noreply.github.com>
2022-09-28 08:29:48 +0530

    v1.0
    Initial stage building the basic template
    and style

---
+++ LibApp/views.py
@@ -0,0 +1,14 @@
+from email.mime import image
+from django.shortcuts import render
+import pickle
+import os
+# Create your views here.
+def index(request):
+    with open('popular1.pkl', 'rb') as f:
+
+        popular_df=pickle.load(f)
+    context_dic={
+        'imgUrl':list(popular_df['Image-URL-L']),
+    }
+
+    return render(request,'index.html',context=context_dic)
